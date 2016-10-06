a79cdaa3e8edf6532bc61db05e2dc7fe67e011d1
kritsada <kritsada.swe@gmail.com>
2016-10-06 09:19:08 +0700

    creat iframe

--- backend/modules/clip/views/listclip/list_clip.php
+++ backend/modules/clip/views/listclip/list_clip.php
@@ -15,31 +15,39 @@ $this->title = 'คลิป';
 <?php foreach( $model as $list) :
   $img = $list->clip['cover_img_id'];
     $poster = CoverImg::find()->where(['id'=>$img])->one();?>
-    <div>
-      <p>
-        <?=  Html::a($list->clip['title'], ['/clip/clip/play_clip','clip_id'=>$list->clip['id']], ['class' => 'btn btn-primary'])?>
-      </p>
+    <div class="box box-success box-solid">
+      <table >
+        <tr>
+
+          <p>
+              <?=  Html::a($list->clip['title'], ['/clip/clip/play_clip','clip_id'=>$list->clip['id']], ['class' => 'btn btn-primary'])?>
+          </p>
+
+            <?= VideoJsWidget::widget([
+                'options' => [
+                    'class' => 'video-js vjs-default-skin vjs-big-play-centered',
+                    'poster' =>'uploads/coverimage/'.$poster->filename,
+                    'width' => '400',
+                    'height' => '200',
+                    'controls' => true,
+                ],
+                'jsOptions' => [
+                    'preload' => 'auto',
+                ],
+                'tags' => [
+                    'source' => [
+                         ['src' =>$list->clip['link'], 'type' => 'video/mp4'],
+                    ],
+                    // 'track' => [
+                    //     ['kind' => 'captions', 'src' => 'http://vjs.zencdn.net/vtt/captions.vtt', 'srclang' => 'en', 'label' => 'English']
+                    // ]
+                ]
+            ]); ?>
+
+        </tr>
+      </table>
 
 
-<?= VideoJsWidget::widget([
-    'options' => [
-        'class' => 'video-js vjs-default-skin vjs-big-play-centered',
-        'poster' =>'uploads/coverimage/'.$poster->filename,
-        'width' => '400',
-        'height' => '200',
-        'controls' => true,
-    ],
-    'jsOptions' => [
-        'preload' => 'auto',
-    ],
-    'tags' => [
-        'source' => [
-             ['src' =>$list->clip['link'], 'type' => 'video/mp4'],
-        ],
-        // 'track' => [
-        //     ['kind' => 'captions', 'src' => 'http://vjs.zencdn.net/vtt/captions.vtt', 'srclang' => 'en', 'label' => 'English']
-        // ]
-    ]
-]); ?>
+
 </div><br>
 <?php  endforeach ;?>
